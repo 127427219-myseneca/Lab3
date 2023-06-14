bd805a8a442e761b2cf0a80b0fd6ebe4f700d200
vikas-singh-06 <06vikaschauhan@gmail.com>
2023-06-14 18:07:19 -0400

    Updated Hello World finction

--- Lab3.py
+++ Lab3.py
@@ -1,10 +1,4 @@
-def write_text_file():
-    name = input("Enter your name: ")
-    file_name = "output.txt"
+def helloWorld():
+    print('Hello World')
 
-    with open(file_name, "w") as file:
-        file.write(name)
-
-    print(f"A text file '{file_name}' has been created with your name.")
-
-write_text_file()
+helloWorld()
